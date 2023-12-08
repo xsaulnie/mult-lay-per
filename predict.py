34563fb86c189f43729247a94749f8927bb9afd3
Xavier SAULNIER <xsaulnie@paul-f3Br3s7.clusters.42paris.fr>
2023-12-08 13:42:06 +0100

    parcing of prediction programm

--- predict.py
+++ predict.py
@@ -2,6 +2,8 @@ import pickle
 import pandas as pd
 import numpy as np
 import argparse
+from metrics import *
+from model import *
 
 def load_csv(name):
 
@@ -24,30 +26,63 @@ def normalize_data(array, train):
     return res
 
 
+def parse_arguments () -> tuple:
+    try:
+        parser = argparse.ArgumentParser(
+            prog='predict.py',
+            description="A program that gives the prediction of a dataset through the neural network model saved in saved_model.npy and evaluate its performance."
+        )
+        parser.add_argument(
+            "-d", "--dataset",
+            dest="dataset_path",
+            type=str,
+            help="path to the testing dataset.",
+            default="data_test.csv"
+        )
+        args = parser.parse_args()
+        return (args.dataset_path)
+    except Exception as e:
+        print("Error parsing arguments: ", e)
+        exit(1)
+
 if __name__ == '__main__':
 
-    file = open('./saved_model.npy', 'rb')
-    mynetwork = pickle.load(file)
-    file.close()
+    try:
+        file = open('./saved_model.npy', 'rb')
+        mynetwork = pickle.load(file)
+        file.close()
+    except:
+        print("Error loading model")
+        exit(1)
 
-    test = load_csv("data_test.csv")
+    if not isinstance(mynetwork, model):
+        print("Error loading model")
+        exit(1)
+
+    d_path = parse_arguments()
+
+    test = load_csv(d_path)
     train = load_csv("data_train.csv")
+
+    if (test is None or train is None):
+        print("Error reading datasets")
+        exit(1)
 
     X_ref = train.loc[:, 2:].to_numpy()
 
     X_test = test.loc[:, 2:].to_numpy()
     X_test = normalize_data(X_test, X_ref)
     Y_test = test.loc[:, 1].to_numpy()
+
     Y_test = np.array([[1, 0] if i == 'M' else [0, 1] for i in Y_test])
 
-
-
     pred_test = mynetwork.predict(X_test)
+
     rounded = [[round(p[0], 3), round(p[1], 3)] for p in pred_test]
-    lossbce = mynetwork.lossbce(np.array([[1, 0] if p[0] > p[1] else [0, 1] for p in pred_test]), Y_test)
-    # print(pred_test)
-    # print(rounded)
-    # exit(0)
+    lossbce = mynetwork.lossbce(pred_test, Y_test)
+    lossmse = mynetwork.lossmse(pred_test, Y_test)
+    (prec, reca, f1) = getmetrics(Y_test, pred_test)
+
     truth = test.loc[:, 1].to_numpy()
     truth = np.array([1 if i == 'M' else 0 for i in truth])
     pred = [1 if p[0] > p[1] else 0 for p in pred_test]
@@ -57,5 +92,9 @@ if __name__ == '__main__':
         if (pred[i] == truth[i]):
             correct=correct + 1
     print("> correctly predicted {}/{}".format(correct, pred_test.shape[0]))
-    print("> performance : ", correct / pred_test.shape[0] * 100, "%")
-    print("> loss (binary crossentropy) : ", lossbce)
+    print("> performance :", correct / pred_test.shape[0] * 100, "%")
+    print("> loss (binary crossentropy) :", lossbce)
+    print("> loss (mean square error) :", lossmse)
+    print("> precision :", prec)
+    print("> recall :", reca)
+    print("> f1_score :", f1)
